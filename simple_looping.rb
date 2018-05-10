998bd7c497891e8bacf75656df2ae38d4ccf757c
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 20:01:40 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,10 +4,11 @@ def loop_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
-    counter +=1
     puts phrase
-    if counter <= number_of_times
+    counter +=1
+    if counter == number_of_times
       break
+    end
   end
 end
 
@@ -39,7 +40,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number in number_of_times do
+  for value in 1..number_of_times do
     puts phrase
   end
 end
