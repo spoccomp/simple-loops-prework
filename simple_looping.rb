c682fa8b95c0ee84b508acd99f005d6a21ca81ac
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 19:57:27 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,8 +39,8 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  #phrase = "Welcome to Flatiron School's Web Development Course!"
-  for shoe in number_of_times do
-    puts "Welcome to Flatiron School's Web Development Course!"
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  for value in number_of_times do
+    puts phrase
   end
 end
