73129673389ef6427510eb91ece2f4ca7eb703c3
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 20:00:28 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,7 +40,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for value in number_of_times do
+  for value in 1..number_of_times do
     puts phrase
   end
 end
